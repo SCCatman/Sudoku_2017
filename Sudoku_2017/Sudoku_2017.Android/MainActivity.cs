eedfd96121fad1923b05659f1c32c7e1c9394d16
Dennis Hayes <dennisdotnet@yahoo.com>
2017-06-29 02:43:49 -0400

    Added gird xaml

--- Sudoku_2017/Sudoku_2017.Android/MainActivity.cs
+++ Sudoku_2017/Sudoku_2017.Android/MainActivity.cs
@@ -10,12 +10,15 @@ using Android.OS;
 namespace Sudoku_2017.Droid
 {
     [Activity(Label = "Sudoku_2017", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    // Needed to change FormsAppCompatActivity to the older FormsApplicationActivity, otherwise buttons won't fill in grid cells
+    //public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity.
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+            // Had to remove after downgrading from FormsAppCompatActivity to FormsApplicationActivity. not what we want to use anyway
+            //TabLayoutResource = Resource.Layout.Tabbar;
+            //ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
 
